ece97b310cb9c45d218e438509c6f22c1efe37a6
linirini <2001yerin@naver.com>
2023-12-14 23:36:58 +0900

    feat : implement level enum

---
+++ src/main/java/pairmatching/util/Feature.java
@@ -0,0 +1,26 @@
+package pairmatching.util;
+
+public enum Feature {
+
+    CAR_RACING("자동차 경주"),
+    LOTTO("로또"),
+    BASEBALL("숫자야구게임"),
+    SHOPPING_BAG("장바구니"),
+    PAYMENT("결제"),
+    SUBWAY_PATH("지하철노선도"),
+    PERFORMANCE("성능개선"),
+    DISTRIBUTION("배포"),
+    NO_FEATURE("없음");
+
+
+    private String viewName;
+
+    Feature(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+}
--- src/main/java/pairmatching/util/Level.java
+++ src/main/java/pairmatching/util/Level.java
@@ -1,12 +1,39 @@
 package pairmatching.util;
 
+import static pairmatching.util.Feature.BASEBALL;
+import static pairmatching.util.Feature.CAR_RACING;
+import static pairmatching.util.Feature.DISTRIBUTION;
+import static pairmatching.util.Feature.LOTTO;
+import static pairmatching.util.Feature.NO_FEATURE;
+import static pairmatching.util.Feature.PAYMENT;
+import static pairmatching.util.Feature.PERFORMANCE;
+import static pairmatching.util.Feature.SHOPPING_BAG;
+import static pairmatching.util.Feature.SUBWAY_PATH;
+
 import java.util.List;
 
 public enum Level {
 
+    LEVEL1("레벨1", List.of(CAR_RACING, LOTTO, BASEBALL)),
+    LEVEL2("레벨2", List.of(SHOPPING_BAG, PAYMENT, SUBWAY_PATH)),
+    LEVEL3("레벨3", List.of(NO_FEATURE)),
+    LEVEL4("레벨4", List.of(PERFORMANCE, DISTRIBUTION)),
+    LEVEL5("레벨5", List.of(NO_FEATURE)),
     ;
 
     private String viewName;
-    private List<String> features;
+    private List<Feature> features;
 
+    Level(String viewName, List<Feature> features) {
+        this.viewName = viewName;
+        this.features = features;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
 }
